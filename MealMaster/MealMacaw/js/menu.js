1ca9e5fa19a2fc990ccc350bce6aa7a2b2d82aa0
igodzp <saranyu_phusit@hotmail.com>
2014-04-12 23:18:24 -0500

    Adding Wiley

--- MealMaster/MealMacaw/js/menu.js
+++ MealMaster/MealMacaw/js/menu.js
@@ -1,14 +1,14 @@
-var breakfast = new Array();
-var lunch = new Array();
-var dinner = new Array();
+var breakfast = new Array(5);
+var lunch = new Array(5);
+var dinner = new Array(5);
 
-var breakfast_category =new Array();
-var lunch_category = new Array();
-var dinner_category = new Array();
+var breakfast_category =new Array(5);
+var lunch_category = new Array(5);
+var dinner_category = new Array(5);
 
-var breakfast_item =new Array();
-var lunch_item = new Array();
-var dinner_item = new Array();
+var breakfast_item =new Array(5);
+var lunch_item = new Array(5);
+var dinner_item = new Array(5);
 
 
 var current = 2;
@@ -25,20 +25,21 @@ var dining_court=['earhart',
                   'hillenbrand',
                   'windsor',                  
                  ];
+                  
+var dining; 
 $.ajax({
     
-	url: dining_court[0]+'.xml',
+	url: dining_court[parseInt(localStorage.getItem("dining"))]+'.xml',
 	dataType: 'xml',
-    
-    
 	
 	success: function(data)
 	{
-	get_breakfast(data);
-	get_lunch(data);	
-    get_dinner(data);
-	show_lunch();
-    localStorage.setItem("currentTab",1);
+        var x = parseInt(localStorage.getItem("dining"));
+	    get_breakfast(data,x);
+	    get_lunch(data,x);	
+        get_dinner(data,x);
+	    show_lunch();
+        localStorage.setItem("currentTab",1);
 	},
 	
 	error: function() {
@@ -75,73 +76,82 @@ $.ajax({
 
 function show_breakfast()
 {
-    for (i=0; i<breakfast.length; i++)
+    var x = parseInt(localStorage.getItem("dining"));
+    for (i=0; i<breakfast[x].length; i++)
     {
-    if (breakfast_category.indexOf( breakfast[i] )!=-1)
-    $( ".viewport" ).append( "<div class=\"meal-section\"><p class=\"sectiontext\">"+breakfast[i]+"</p><div class=\"meal-section-text\"></div></div>");
+    if (breakfast_category[x].indexOf( breakfast[x][i] )!=-1)
+    $( ".viewport" ).append( "<div class=\"meal-section\"><p class=\"sectiontext\">"+breakfast[x][i]+"</p><div class=\"meal-section-text\"></div></div>");
         else
-    $( ".viewport" ).append( "<div onClick=\"alerts("+i+");\" class=\"dishcontainer d"+i.toString+"\" href='dish.html'><p class=\"dishtext\">"+breakfast[i]+"</p><div class=\"dishbg\"></div</div>" );   
+    $( ".viewport" ).append( "<div onClick=\"alerts("+i+");\" class=\"dishcontainer d"+i.toString+"\" href='dish.html'><p class=\"dishtext\">"+breakfast[x][i]+"</p><div class=\"dishbg\"></div</div>" );   
     }
 }
 
 function show_lunch()
 {
-    for (i=0; i<lunch.length; i++)
+    var x = parseInt(localStorage.getItem("dining"));
+    for (i=0; i<lunch[x].length; i++)
     {
-    if (lunch_category.indexOf( lunch[i] )!=-1)
-    $( ".viewport" ).append( "<div class=\"meal-section\"><p class=\"sectiontext\">"+lunch[i]+"</p><div class=\"meal-section-text\"></div></div>");
+    if (lunch_category[x].indexOf( lunch[x][i] )!=-1)
+    $( ".viewport" ).append( "<div class=\"meal-section\"><p class=\"sectiontext\">"+lunch[x][i]+"</p><div class=\"meal-section-text\"></div></div>");
         else
-    $( ".viewport" ).append( "<div onClick=\"alerts("+i+");\" class=\"dishcontainer d"+i.toString+"\" href='dish.html'><p class=\"dishtext\">"+lunch[i]+"</p><div class=\"dishbg\"></div</div>" );   
+    $( ".viewport" ).append( "<div onClick=\"alerts("+i+");\" class=\"dishcontainer d"+i.toString+"\" href='dish.html'><p class=\"dishtext\">"+lunch[x][i]+"</p><div class=\"dishbg\"></div</div>");   
     }
 }
 
 function show_dinner()
 {
-    for (i=0; i<dinner.length; i++)
+    var x = parseInt(localStorage.getItem("dining"));
+    for (i=0; i<dinner[x].length; i++)
     {
-    if (dinner_category.indexOf( dinner[i] )!=-1)
-    $( ".viewport" ).append( "<div class=\"meal-section\"><p class=\"sectiontext\">"+dinner[i]+"</p><div class=\"meal-section-text\"></div></div>");
+    if (dinner_category[x].indexOf( dinner[x][i] )!=-1)
+    $( ".viewport" ).append( "<div class=\"meal-section\"><p class=\"sectiontext\">"+dinner[x][i]+"</p><div class=\"meal-section-text\"></div></div>");
         else
-    $( ".viewport" ).append( "<div onClick=\"alerts("+i+");\" class=\"dishcontainer d"+i.toString+"\" href='dish.html'><p class=\"dishtext\">"+dinner[i]+"</p><div class=\"dishbg\"></div</div>" );   
+    $( ".viewport" ).append( "<div onClick=\"alerts("+i+");\" class=\"dishcontainer d"+i.toString+"\" href='dish.html'><p class=\"dishtext\">"+dinner[x][i]+"</p><div class=\"dishbg\"></div</div>" );   
     }
 }
 
 
-function get_breakfast(data)
-    {
+function get_breakfast(data,x)
+{
+    breakfast[x] = new Array();
+    breakfast_item[x] = new Array();
+    breakfast_category[x] = new Array();
     $(data).find('Menu Breakfast MenuSection Name').each(function()
 	{
 		
 		var item = $(this).text();
-		breakfast.push(item);
+		breakfast[x].push(item);
 		
 	});
 	
 	$(data).find('Menu Breakfast MenuSection MenuItem Name').each(function()
 	{			
 		var item = new menu_item($(this).text(),$(this).next().text(),0);	
-		breakfast_item.push(item);
+		breakfast_item[x].push(item);
 		
 	});
 	
 	for (i=0;i<breakfast.length;i++)
 	{
 		var flag=false;
-		for (j=0; j<breakfast_item.length; j++)
-		 if (breakfast_item[j].name==breakfast[i]) flag=true;
+		for (j=0; j<breakfast_item[x].length; j++)
+		 if (breakfast_item[x][j].name==breakfast[x][i]) flag=true;
 		if (!flag) 
         {
-            breakfast_category.push(breakfast[i]);
+            breakfast_category[x].push(breakfast[i]);
         }
 	}    
     }
 
-function get_lunch(data)
+function get_lunch(data,x)
 {
-$(data).find('Menu Lunch MenuSection Name').each(function() //all the name
+    lunch[x] = new Array();
+    lunch_item[x] = new Array();
+    lunch_category[x] = new Array();
+    $(data).find('Menu Lunch MenuSection Name').each(function() //all the name
 	{
 		var item = $(this).text();
-		lunch.push(item);       
+		lunch[x].push(item);       
 		
 	});
 	
@@ -149,31 +159,34 @@ $(data).find('Menu Lunch MenuSection Name').each(function() //all the name
 	{
 		
 		var item = new menu_item($(this).text(),$(this).next().text(),0);	
-		lunch_item.push(item);
+		lunch_item[x].push(item);
         if (localStorage.getItem(item.name)==null)
-        localStorage.setItem(item.name,item.score);
-			});
+            localStorage.setItem(item.name,item.score);
+    });
         
     for (i=0;i<lunch.length;i++)
 	{
 		var flag=false;
-		for (j=0; j<lunch_item.length; j++)
-		 if (lunch_item[j].name==lunch[i]) flag=true;
+		for (j=0; j<lunch_item[x].length; j++)
+		 if (lunch_item[x][j].name==lunch[x][i]) flag=true;
 		if (!flag) 
         {
-            lunch_category.push(lunch[i]);
+            lunch_category[x].push(lunch[i]);
         }
 	}
 }
 
 
 
-function get_dinner(data)
+function get_dinner(data,x)
 {
-$(data).find('Menu Dinner MenuSection Name').each(function() //all the name
+    dinner[x] = new Array();
+    dinner_item[x] = new Array();
+    dinner_category[x] = new Array();
+    $(data).find('Menu Dinner MenuSection Name').each(function() //all the name
 	{
 		var item = $(this).text();
-		dinner.push(item);       
+		dinner[x].push(item);       
 		
 	});
 	
@@ -181,7 +194,7 @@ $(data).find('Menu Dinner MenuSection Name').each(function() //all the name
 	{
 		
 		var item = new menu_item($(this).text(),$(this).next().text(),0);	
-		dinner_item.push(item);
+		dinner_item[x].push(item);
         if (localStorage.getItem(item.name)==null)
         localStorage.setItem(item.name,item.score);
 			});
@@ -189,11 +202,11 @@ $(data).find('Menu Dinner MenuSection Name').each(function() //all the name
     for (i=0;i<dinner.length;i++)
 	{
 		var flag=false;
-		for (j=0; j<dinner_item.length; j++)
-		 if (dinner_item[j].name==dinner[i]) flag=true;
+		for (j=0; j<dinner_item[x].length; j++)
+		 if (dinner_item[x][j].name==dinner[x][i]) flag=true;
 		if (!flag) 
         {
-            dinner_category.push(dinner[i]);
+            dinner_category[x].push(dinner[i]);
         }
 	}
 }
